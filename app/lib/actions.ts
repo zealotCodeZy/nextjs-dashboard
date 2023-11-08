8eb6312f8616e87573b51816f297065d96ab688b
zealotCodeZy <461703952@qq.com>
2023-11-08 15:48:39 -0800

    11/8 add authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
  
 const InvoiceSchema = z.object({
   id: z.string(),
@@ -30,6 +31,20 @@ export type State = {
     message?: string | null;
   };
  
+export async function authenticate(
+prevState: string | undefined,
+formData: FormData,
+) {
+try {
+    await signIn('credentials', Object.fromEntries(formData));
+} catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+    return 'CredentialSignin';
+    }
+    throw error;
+}
+}
+
 export async function createInvoice(prevState: State, formData: FormData) {
     const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
